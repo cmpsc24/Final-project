df66b07a07f770c2a15864916c4fc59d351ddae4
cmpsc24 <156448322+cmpsc24@users.noreply.github.com>
2024-04-22 11:58:00 -0400

    election.py

--- election.py
+++ election.py
@@ -8,7 +8,6 @@ class Voter:
         self.state = state
         self.id_number = id_number
 
-
     def vote(self, election):
         voter_data = {
             "name": self.name,
@@ -19,15 +18,12 @@ class Voter:
         }
         election.add_voter(voter_data)
 
-
 class Election:
     def __init__(self):
         self.voters = []
 
-
     def add_voter(self, voter_data):
         self.voters.append(voter_data)
-
 
     def display_voters(self):
         for i in self.voters:
@@ -39,13 +35,9 @@ def display_election_results(election):
     oldest_voter = max(voter["age"] for voter in election.voters)
     age_range = f"{youngest_voter}-{oldest_voter}"
 
-
-
-
     # Calculate average age
     total_age = sum(voter["age"] for voter in election.voters)
     average_age = total_age / len(election.voters)
-
 
     # Count votes for each party
     party_votes = {}
@@ -54,7 +46,6 @@ def display_election_results(election):
         if party not in party_votes:
             party_votes[party] = 0
         party_votes[party] += 1
-
 
     # Display results
     print("Election Results:")
@@ -67,9 +58,7 @@ voter_ages = [random.randint(16, 80) for _ in range(30)]
 voter_parties = ["Donut", "Ice Cream"]
 voter_states = ["CA", "TX", "FL", "NY", "PA", "IL", "OH", "MI", "NC", "GA"]
 
-
 voters = []
-
 
 for i in range(5):
     name = random.choice(voter_names)
@@ -80,7 +69,6 @@ for i in range(5):
     voter = Voter(name, age, party, state, id_number)
     # voters.append(voter)
 
-
     # Check if voter is at least 18 years old
     if age >= 18:
         voter = Voter(name, age, party, state, id_number)
@@ -88,21 +76,15 @@ for i in range(5):
     else:
         print(f"{name} is not eligible to vote because they are under 18 years old.")
 
-
 # Example usage:
 #added voters to election
 
-
 election = Election()
-
 
 while voters:
     v = voters.pop(0)  # dequeue the first voter
     v.vote(election)  # voter votes
 
-
 # print("Election results:", election.voters)
 election.display_voters()
 display_election_results(election) #changed from election
-
-
